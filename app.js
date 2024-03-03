c4723317c9e083d3df1895664cec86f86be14ede
satyam1906 <satyam9322@gmail.com>
2024-03-03 23:41:40 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const colors=["green","red","rgba(133,122,200)","#f15025"]
+const btn = document.getElementById(`btn`);
+const color= document.getElementsByClassName(`color`);
+
+btn.addEventListener('click',()=>{
+    // console.log(document.body);
+    // get random number between 0-3
+    const randomNumber =getRandomNumber()  ;
+    console.log(randomNumber) ;
+    document.body.style.backgroundColor = colors[randomNumber];
+
+})
+
+
+getRandomNumber= ()=>{
+    return Math.random() ;
+}
