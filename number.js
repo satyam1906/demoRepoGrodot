2c368028b1371f4d69727f83892cd59061ecf01d
satyam1906 <satyam9322@gmail.com>
2024-03-04 15:17:39 +0530

    Added addition method to number.js

--- number.js
+++ number.js
@@ -1,16 +1,16 @@
-function evenOddProgram(n){
-    
-        if(n%2 == 0){
-            console.log("even");
-        }else{
-            console.log("Odd");
-        }
-    
-    
-} 
+function evenOddProgram(n) {
+  if (n % 2 == 0) {
+    console.log('even');
+  } else {
+    console.log('Odd');
+  }
+}
 
+let addition = (a, b) => {
+  return a + b;
+};
 
 const number = evenOddProgram(45);
 console.log(number);
 
-
+console.log(addition(2,7));
